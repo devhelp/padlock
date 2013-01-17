ae4bfeb0afe1d011f355b07a723e3b506cfada5b
pawel <devhelp@devhelp.pl>
2013-01-17 18:56:10 +0100

    index file added

--- demo/index.php
+++ demo/index.php
@@ -2,4 +2,14 @@
 
 require_once __DIR__.'/autoload.php';
 
-die('ssss');
+$filename = 'padlock.lock';
+
+$padlock = new \Devhelp\Padlock\FilePadlock($filename);
+
+echo "padlock - is locked: ".($padlock->isLocked() ? 'true' : 'false')."\n";
+echo "locking the padlock...\n";
+$padlock->lock();
+echo "padlock - is locked: ".($padlock->isLocked() ? 'true' : 'false')."\n";
+echo "unlocking the padlock...\n";
+$padlock->unlock();
+echo "padlock - is locked: ".($padlock->isLocked() ? 'true' : 'false')."\n";
